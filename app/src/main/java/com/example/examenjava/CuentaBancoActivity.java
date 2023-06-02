0e05437aa559b88f511772c901aa08e02c9b72c1
Maxwell1705 <contrerasmaximiliano72@gmail.com>
2023-06-02 12:21:04 -0600

    Se agrego una nueva actividad cuentaBancoActivity y se hizo el diseño

---
+++ app/src/main/java/com/example/examenjava/CuentaBancoActivity.java
@@ -0,0 +1,15 @@
+package com.example.examenjava;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CuentaBancoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cuenta_banco);
+    }
+}
+
